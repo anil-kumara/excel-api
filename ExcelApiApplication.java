8436e73413bab25d569b3c095188b5a4997018f9
Anilkummara <129746312+anil-kumara@users.noreply.github.com>
2023-05-17 17:36:37 +0530

    API runing point

---
+++ ExcelApiApplication.java
@@ -0,0 +1,13 @@
+package com.excelToMysql.Excel.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelApiApplication.class, args);
+	}
+
+}
